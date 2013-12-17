afdbe49d7cc0d9455a30fae189b05975e9f590a0
sarahalhawi92 <shawi3333@gmail.com>
2013-12-17 21:53:59 +0000

    working on reset password page

--- homepage/registration & login/resetpassword.php
+++ homepage/registration & login/resetpassword.php
@@ -10,19 +10,22 @@ header('Content-type: text/html; charset=utf-8');
       // Clear the error message
 $error_msg = "";
 
+echo ggfdg;
 
 if (isset($_POST['submit'])) { // Handle the form.
 
 	$dbc = mysqli_connect('localhost', 'root', 'root', 'help_me_be_healthy') or die("Error " . mysqli_error($dbc));
 	mysqli_set_charset($dbc, "utf8");
 
+	echo blah;
+
 	if (empty($_POST['email_address'])) { // Validate the email address.
 
-		echo fdfg;
+		echo blah2;
 
-		$user_id = FALSE;
+//$uid = FALSE;
 
-		echo ‘<p><font color=”red” size=”+1″>You forgot to enter your email address!</font></p>’;
+//echo ‘<p><font color=”red” size=”+1″>You forgot to enter your email address!</font></p>’;
 
 //Checks if the form below has been submitted. Then it checks that an email address has been entered with the empty() function. If not we warn the visitor.
 
@@ -30,88 +33,79 @@ if (isset($_POST['submit'])) { // Handle the form.
 
 // Check for the existence of that email address.
 
-		$query = "SELECT `user_id` FROM `users` WHERE `email_address` = ’”.  ($_POST['email_address']) . “‘";
+
+		$query = "SELECT `user_id` FROM `users` WHERE `email_address`= '" . $_POST['email_address'] . "' ";
+
+		$data= mysqli_query($dbc,$query) or die('Query failed: ' . mysql_error());
 
 
-		$result = mysql_query ($query) or trigger_error(“Query: $query\n<br />MySQL Error: ” . mysql_error());
+//Here we are checking that the email address they entered is valid. The query asks for a user_id, form the table users, where the value is equal to the email entered. The escape_data() function cleans up the information entered. 
 
-//Here we are checking that the email address they entered is valid. The query asks for a user_id, form the table users, where the value is equal to the email entered. The escape_data() function cleans up the information entered. For more information on this function, refer to the previous PHP Tutorial.
+		if (mysqli_num_rows($data) == 1) {
 
-		if (mysql_num_rows($result) == 1) {
+			echo blah3;
 
 // Retrieve the user ID.
 
-			list($uid) = mysql_fetch_array ($result, MYSQL_NUM);
+			$row = mysqli_fetch_assoc($data);
+			$_SESSION['user_id'] = $row['user_id'];
 
-//If there was a positive response from our query, retrieve the userid. We assign the results using the list function, as if they were an array, but we will only get one result, because we only have one account for each email address.
+			$temp_pass = substr ( md5(uniqid(rand(),1)), 3, 10);
 
-		} else {
+			echo $temp_pass;
 
-			echo ‘<p><font color=”red” size=”+1″>The submitted email address does not match those on file!</font></p>’;
+			$query = "UPDATE `users` SET `password` = SHA('$temp_pass') WHERE `user_id` = '" . $_SESSION['user_id'] . "' ";
 
-			$uid = FALSE;
+			mysqli_query($dbc, $query) or die("Error " . mysqli_error($data));
+
+//		} else {
+
+//			echo ‘<p><font color=”red” size=”+1″>The submitted email address does not match those on file!</font></p>’;
+
+//			$user_id = FALSE;
 
 		}
 
 	}
 
-//If the email address wasn’t found with our query, we alert them to that fact.
+          if($query){
 
-if ($uid) { // If everything’s OK.
+          	echo blah5;
 
-// Create a new, random password.
+          	$to=$email_address;
 
-	$p = substr ( md5(uniqid(rand(),1)), 3, 10);
+          	$subject="Reset your Password";
 
-//If the query was successful, we create a new 32 digit password for them. For more information on what these functions do, refer to the previous PHP Tutorial. They basically just create a new random password, for temporary use.
+          	$header="from: helpmebehealthy <helpmebehealthy.com>";
 
-// Make the query.
+          	$message="Your password to log into SITENAME has been temporarily changed to ‘$temp_pass’. Please log in using this password and your email address. ";
 
-	$query = “UPDATE users SET pass=SHA(‘$p’) WHERE user_id=$uid”;
+          	$sentmail = mail($to,$subject,$message,$header);
 
-	$result = mysql_query ($query) or trigger_error(“Query: $query\n<br />MySQL Error: ” . mysql_error());
+          }
 
-//This query updates the record in the database, by setting the new password based on the record having the value of the entered email address.
+           // if your email succesfully sent
+          if($sentmail){
 
-if (mysql_affected_rows() == 1) { // If it ran OK.
+           echo "Your Confirmation link Has Been Sent To Your Email Address.";
 
-// Send an email.
+          }
 
-	$body = “Your password to log into SITENAME has been temporarily changed to ‘$p’. Please log in using this password and your username. At that time you may change your password to something more familiar.”;
+          else {
 
-	mail ($_POST['email'], ‘Your temporary password.’, $body, ‘From: admin@sitename.com’);
+            echo "Cannot send  link to your e-mail address";
 
-	echo ‘<h3>Your password has been changed. You will receive the new, temporary password at the email address with which you registered. Once you have logged in with this password, you may change it by clicking on the “Change Password” link.</h3>’;
+} // Confirm success with the user
+//echo '<p>Your password has been reset.</p>';
 
-	Here we check that the update query was successful, with the mysql_affected_rows() function. If it was we create a message to send to the visitor and then mail them their new password.
 
-// mysql_close(); // Close the database connection.
+//mysqli_close($dbc);
 
-include (‘./includes/footer.html’); // Include the HTML footer.
-
-// exit();
-
-//Here we are closing the database connection and the script.
-
-} else { // If it did not run OK.
-
-	echo ‘<p><font color=”red” size=”+1″>Your password could not be changed due to a system error. We apologize for any inconvenience.</font></p>’;
-
-}
-
-} else { // Failed the validation test.
-
-	echo ‘<p><font color=”red” size=”+1″>Please try again.</font></p>’;
-
-}
-
-//Here we are alerting the visitor that we couldn’t change their password, because of either a system error or because they entered the wrong information.
+//exit();
 
 
 
 
-
-
-} // End of the main Submit conditional.
+} 
 
 ?>
